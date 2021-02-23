b3841b50c4cb16f054f68835233faf3e8365816f
Barnabás Rátki <barnabas.ratki@gmail.com>
2021-02-23 19:02:32 +0100

    WiP

--- enderbank/frontend/main.lua
+++ enderbank/frontend/main.lua
@@ -1,3 +1,16 @@
-print("Dankyer LLC Bank wallet app V 0.0");
 
-print("kek");
+local Api = {
+    url = "http://enderbank.ngrok.io",
+
+    register = function (username, password)
+        print(self.url);
+    end
+};
+
+function main()
+    print("Dankyer LLC Bank wallet-app V 0.0");
+
+    Api:register("asd", "asd");
+
+    print("kek");
+end
