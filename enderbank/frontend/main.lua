ff78f9a76a48c2c1509c4b572525fc1c7a9ec36c
Barnabás Rátki <barnabas.ratki@gmail.com>
2021-02-23 19:39:59 +0100

    Wip3

--- enderbank/frontend/main.lua
+++ enderbank/frontend/main.lua
@@ -2,29 +2,57 @@
 local Ui = {
     askQuestion = function (question,options)
         print(question);
-        for kk, vv in pair(options) do
+        for kk, vv in pairs(options) do
             print("> ["..kk.."] "..vv);
         end
         local answer = nil;
         repeat
             local inp = tonumber(read());
-            if inp < 0 or inp > options:getn() then
+            if inp < 0 or inp > table.getn(options) then
                 print("Invalid option");
             else
                 answer = inp;
             end
-        until(!answer)
+        until(answer ~= nil)
 
         return answer;
     end
 };
 
 local Api = {
-    url = "http://enderbank.ngrok.io",
+    url = "http://enderbank.ngrok.io/",
     token = nil,
 
+    getHeaders = function (authed)
+        if authed then
+            return {
+                Authorization="Bearer "..token,
+            };
+        else
+            return {};
+        end
+    end,
+
+    getUrl = function (path)
+        return Api.url..path;
+    end,
+
+    post = function (path, authed, body)
+        local resp = http.post({ url=Api.getUrl(), headers=Api.getHeaders(authed), body=textutils.serializeJSON(body)});
+        local respJson = textutils.unserializeJSON(resp);
+
+        return respJson;
+    end,
+
+    saveToken = function (tokenData)
+        local tokFile = fs.open("./token", "w");
+        tokFile.write(tokenData);
+        Api.token = tokenData;
+        tokFile.close();
+    end,
+
     loadToken = function ()
-        if !fs.exists("./token") then
+        if not fs.exists("./token") then
             return false;
         end
         local tokFile = fs.open("./token", "r");
@@ -32,17 +60,46 @@ local Api = {
         if token:len() == 0 then
             return false;
         end
+        tokFile.close();
 
-        self.token = token;
+        Api.token = token;
         return true;
-    end
+    end,
 
     register = function (username, password)
-        print(self.url);
+        local resp = Api.post("api/auth/register", false, {
+            userName=username,
+            password=password
+        });
+
+        if resp.statusCode then
+            return false;
+        end
+
+        Api.saveToken(resp.token);
+        return true;
     end
 };
 
-function registerFlow() then
+function registerFlow()
+    print("=========");
+    print("Registering new user.");
+    print("- Username:");
+    local username = read();
+    print("- Password:");
+    local pwd = read("*");
+
+    local didReg = Api.register(username, pwd);
+    if not didReg then
+        print("We couldn't register you, please try again.");
+        registerFlow();
+    else
+        print("wuuh");
+        print(Api.token);
+    end
+end
+
+function unathorizedFlow()
     print("You seem to be logged out.");
     local loginOrRegister = Ui.askQuestion("What to do ?", {"Login", "Register"});
     print(loginOrRegister);
@@ -50,11 +107,11 @@ end
 
 function main()
     print("Dankyer LLC Bank wallet-app V 0.0");
-    local loadedToken = Api:loadedToken();
+    local loadedToken = Api.loadToken();
     if loadedToken then
         print("Welcome back!");
     else
-        registerFlow();
+        unathorizedFlow();
     end
 end
 
